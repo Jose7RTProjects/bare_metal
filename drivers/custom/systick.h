cb4c9c62d3d6aa5f512dbd513f4182fd0c813ea5
Jose7RT <jose7rt@gmail.com>
2025-01-10 21:13:07 +0100

    Added basic systick driver

---
+++ drivers/custom/systick.h
@@ -0,0 +1,8 @@
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include "stm32f4xx.h"
+
+void systick_ms_delay(uint32_t delay);
+
+#endif // SYSTICK_H_
--- src/main.c
+++ src/main.c
@@ -1,24 +1,15 @@
 #include "led.h"
-#include "button.h"
+#include "systick.h"
 
 int btn_state;
 
 int main(void)
 {
     led_init();
-    button_init();
 
     while(1)
     {
-        btn_state = get_button_state();
-        
-        if (btn_state)
-        {
-            led_on();
-        }
-        else
-        {
-            led_off();
-        }
+        led_toggle();
+        systick_ms_delay(10000);
     }
 }
